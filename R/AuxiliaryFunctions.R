5b67cdabf2d9f9bb5816c6d74e32b156db2c058d
FranziskaBentz <106803133+FranziskaBentz@users.noreply.github.com>
2024-06-07 14:32:10 +0300

    Update AuxiliaryFunctions.R

--- R/AuxiliaryFunctions.R
+++ R/AuxiliaryFunctions.R
@@ -11,9 +11,6 @@
 #' @examples
 #' fastq.process("data.fastq", "CCGGT[ATCG]{8}GAATTC", "CCGGT", "GAATTC")
 #' 
-hello <- function() {
-  print("Franzi")
-}
 
 fastq.process <- function(fastq.file, pattern, short.nt.before.tag, short.nt.after.tag) {
   con <- file(fastq.file, "r")
